efcd9dd05b7a9198eacbe365ef90d634192541ad
JazepsIvulis <jazeps.ivulis@gmail.com>
2023-04-04 21:54:55 +0300

    Adding logic for switching between light and dark themes

--- DarkTheme/Controller/ThemeViewController.swift
+++ DarkTheme/Controller/ThemeViewController.swift
@@ -8,29 +8,50 @@
 import UIKit
 
 class ThemeViewController: UIViewController {
-
+    
     @IBOutlet weak var darkThemeButton: UIButton!
-    var mainText: String = "Dark Theme On"
-    var darkIsOn: Bool = false
+    
+    enum NavBarText: String {
+        case off = "Dark Theme Off"
+        case on = "Dark Theme On"
+    }
+    
+    enum ButtonText: String {
+        case off = "Turn Dark Theme Off"
+        case on = "Turn Dark Theme On"
+    }
+
+    var darkThemeIsOn: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad from ViewController")
         darkThemeButton.layer.cornerRadius = 8
-        // Do any additional setup after loading the view.
+        navigationItem.title = NavBarText.off.rawValue
+        darkThemeButton.setTitle(ButtonText.on.rawValue, for: .normal)
     }
 
     @IBAction func darkThemeButtonTapped(_ sender: Any) {
-        darkThemeIsOn(isOn: darkIsOn)
+        turnDarkThemeOn(isDarkThemeOn: darkThemeIsOn)
     }
     
-    func darkThemeIsOn(isOn: Bool) {
-        darkThemeButton.setTitle(mainText, for: .normal)
-        darkThemeButton.setTitleColor(UIColor.black, for: .normal)
-        
-        navigationItem.title = mainText
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
-        view.backgroundColor = UIColor.black
+    func turnDarkThemeOn(isDarkThemeOn: Bool) {
+        if darkThemeIsOn {
+            darkThemeButton.setTitle(ButtonText.on.rawValue, for: .normal)
+            darkThemeButton.setTitleColor(UIColor.black, for: .normal)
+            
+            navigationItem.title = NavBarText.off.rawValue
+            navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+            view.backgroundColor = UIColor.white
+            darkThemeIsOn = false
+        } else {
+            darkThemeButton.setTitle(ButtonText.off.rawValue, for: .normal)
+            darkThemeButton.setTitleColor(UIColor.white, for: .normal)
+            
+            navigationItem.title = NavBarText.on.rawValue
+            navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+            view.backgroundColor = UIColor.black
+            darkThemeIsOn = true
+        }
     }
 }
 
